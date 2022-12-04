d8e94a97b367585f753bcd009a11be834dffeef8
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-04 23:10:48 +0100

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { OneComponent } from './one/one.component';
 import { TwoComponent } from './two/two.component';
 
+// Load a component for each route.
 const routes: Routes = [
   { path: 'one', component: OneComponent },
   { path: 'two', component: TwoComponent },
